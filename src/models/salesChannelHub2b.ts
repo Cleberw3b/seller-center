f0f657ceaf5c781da19cf931e37729487b1c0b3d
Cleber Oliveira <cleberw3b@gmail.com>
2021-09-13 14:44:28 -0300

    delete product on hub2b

---
+++ src/models/salesChannelHub2b.ts
@@ -0,0 +1,248 @@
+
+export const SALES_CHANNEL_HUB2B = [
+    {
+        code: 2,
+        ticker: "NVP",
+        name: "Cnova"
+    },
+    {
+        code: 4,
+        ticker: "mrc",
+        name: "MercadoLivre"
+    },
+    {
+        code: 7,
+        ticker: "amz",
+        name: "Amazon"
+    },
+    {
+        code: 10,
+        ticker: "bus",
+        name: "Buscape"
+    },
+    {
+        code: 12,
+        ticker: "zoo",
+        name: "Zoom"
+    },
+    {
+        code: 41,
+        ticker: "daf",
+        name: "Dafiti"
+    },
+    {
+        code: 46,
+        ticker: "mob",
+        name: "Mobly"
+    },
+    {
+        code: 51,
+        ticker: "nsh",
+        name: "Netshoes"
+    },
+    {
+        code: 71,
+        ticker: "css",
+        name: "CissaMagazine"
+    },
+    {
+        code: 73,
+        ticker: "cfr",
+        name: "Carrefour"
+    },
+    {
+        code: 74,
+        ticker: "fsh",
+        name: "Fastshop"
+    },
+    {
+        code: 75,
+        ticker: "mav",
+        name: "Mercado Aventura"
+    },
+    {
+        code: 76,
+        ticker: "mag",
+        name: "MagazineLuiza"
+    },
+    {
+        code: 77,
+        ticker: "cnt",
+        name: "Centauro"
+    },
+    {
+        code: 78,
+        ticker: "rce",
+        name: " Ricardo Eletro"
+    },
+    {
+        code: 79,
+        ticker: "fnw",
+        name: "Farma Now"
+    },
+    {
+        code: 82,
+        ticker: "sky",
+        name: "B2WSkyHub"
+    },
+    {
+        code: 85,
+        ticker: "cea",
+        name: "CeA"
+    },
+    {
+        code: 86,
+        ticker: "cpt",
+        name: "ConnectParts"
+    },
+    {
+        code: 88,
+        ticker: "vev",
+        name: "Via Evangélica"
+    },
+    {
+        code: 91,
+        ticker: "lml",
+        name: "Leroy Merlin"
+    },
+    {
+        code: 92,
+        ticker: "llt",
+        name: "Luxury Loyalty"
+    },
+    {
+        code: 93,
+        ticker: "ccj",
+        name: "Cidade Jardim"
+    },
+    {
+        code: 94,
+        ticker: "fft",
+        name: "Farfetch"
+    },
+    {
+        code: 96,
+        ticker: "htg",
+        name: "HomeToGo"
+    },
+    {
+        code: 97,
+        ticker: "clb",
+        name: "Colombo"
+    },
+    {
+        code: 99,
+        ticker: "wbc",
+        name: "WebContinental"
+    },
+    {
+        code: 101,
+        ticker: "mam",
+        name: "Madeira Madeira"
+    },
+    {
+        code: 102,
+        ticker: "est",
+        name: "eStyle"
+    },
+    {
+        code: 104,
+        ticker: "liv",
+        name: "LVL"
+    },
+    {
+        code: 107,
+        ticker: "hil",
+        name: "HiLo"
+    },
+    {
+        code: 108,
+        ticker: "coo",
+        name: "Coopera"
+    },
+    {
+        code: 110,
+        ticker: "jsm",
+        name: "JuntosSomosMais Marketplace"
+    },
+    {
+        code: 111,
+        ticker: "nvm",
+        name: "Novo Mundo"
+    },
+    {
+        code: 112,
+        ticker: "mnu",
+        name: "Menu"
+    },
+    {
+        code: 113,
+        ticker: "bof",
+        name: "Bestoff"
+    },
+    {
+        code: 114,
+        ticker: "rhp",
+        name: "RiHappy"
+    },
+    {
+        code: 115,
+        ticker: "eji",
+        name: "Enjoei"
+    },
+    {
+        code: 116,
+        ticker: "hbd",
+        name: "Hibrido"
+    },
+    {
+        code: 119,
+        ticker: "nts",
+        name: "NetSuprimentos"
+    },
+    {
+        code: 120,
+        ticker: "caq",
+        name: "CompreAqui"
+    },
+    {
+        code: 122,
+        ticker: "gpa",
+        name: "GPA"
+    },
+    {
+        code: 123,
+        ticker: "kbm",
+        name: "Kabum"
+    },
+    {
+        code: 124,
+        ticker: "shp",
+        name: "Shopee"
+    },
+    {
+        code: 126,
+        ticker: "ats",
+        name: "AtivoStore"
+    },
+    {
+        code: 128,
+        ticker: "prf",
+        name: "ProntaReforma"
+    },
+    {
+        code: 130,
+        ticker: "ecd",
+        name: "eCadeiras"
+    },
+    {
+        code: 131,
+        ticker: "tcob",
+        name: "TocaObra"
+    },
+    {
+        code: 133,
+        ticker: "rds",
+        name: "RaiaDrogasil"
+    }
+]
