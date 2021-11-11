41cf605c2897c8bcdeb47b0ba81b2905eb38dfda
diogo-1630668567 <diogo.noroes@gmail.com>
2021-11-11 00:16:24 -0300

    feat: criacao de models tenant e users

--- src/repositories/hub2TenantRepository.ts
+++ src/repositories/hub2TenantRepository.ts
@@ -2,7 +2,7 @@
 //      hub2b tenant Repository
 //
 
-import { ObjectID, MongoError } from "mongodb"
+import { MongoError } from "mongodb"
 import { HUB2B_Tenants } from "../models/hub2b"
 import { hub2bTenantCollection } from "../utils/db/collections"
 import { log } from "../utils/loggerUtil"
@@ -16,70 +16,22 @@ import { getFunctionName } from "../utils/util"
 export const saveTenant = async ( tenants: HUB2B_Tenants ): Promise<boolean> => {
 
     try {
-        const result = await hub2bTenantCollection.insertOne( tenants )
 
-        return result.ops[0] ? true : false
+        const replacement = { ...tenants }
+
+        const options = {
+            upsert: true,
+            returnNewDocument: true
+        }
+
+        const result = await hub2bTenantCollection.findOneAndReplace( replacement, options )
+
+        return result.ok ? true : false
 
     } catch ( error ) {
         if ( error instanceof MongoError || error instanceof Error )
             log( error.message, 'EVENT', `HUB2B tenant Repository - ${ getFunctionName() }`, 'ERROR' )
         return false
-    }
-}
-
-/**
- * Update hub2b tenants
- * 
- * @param tenants
- */
- export const updateTenant = async ( tenants: HUB2B_Tenants ): Promise<HUB2B_Tenants | null> => {
-
-    try {
-        const result = await hub2bTenantCollection.updateOne( 
-            { idTenant: tenants.idTenant },
-            { $set: tenants },
-            { upsert: true } )
-
-        let id: ObjectID | null = null
-
-        if ( result.upsertedCount )
-            id = result.upsertedId._id
-    
-        if ( result.matchedCount )
-            return await findHub2bTenantByIdTenant( tenants.idTenant )
-    
-        if ( !id ) throw new MongoError( "Não gerou id" )
-    
-        tenants._id = id
-    
-        return tenants
-
-    } catch ( error ) {
-        if ( error instanceof MongoError || error instanceof Error )
-            log( error.message, 'EVENT', `HUB2B tenant Repository - ${ getFunctionName() }`, 'ERROR' )
-        return null
-    }
-}
-
-/**
- * Find hub2b Tenant by idTenant
- * 
- * @param idTenant
- */
- export const findHub2bTenantByIdTenant = async ( idTenant: number ): Promise<HUB2B_Tenants | null> => {
-
-    try {
-
-        const hub2bTenant = await hub2bTenantCollection.findOne( { idTenant } )
-
-        return hub2bTenant
-
-    } catch ( error ) {
-
-        if ( error instanceof MongoError || error instanceof Error )
-            log( error.message, 'EVENT', `HUB2B tenant Repository - ${ getFunctionName() }`, 'ERROR' )
-
-        return null
     }
 }
 
