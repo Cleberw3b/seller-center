4cd6cb426e1d527823d914262cac766bfecfe032
diogo-1630668567 <diogo.noroes@gmail.com>
2021-11-12 00:24:40 -0300

    feat: add model TenantCredentials

--- src/repositories/hub2TenantCredentialRepository.ts
+++ src/repositories/hub2TenantCredentialRepository.ts
@@ -17,9 +17,16 @@ export const saveTenantCredential = async ( tenantCredentials: HUB2B_TenantCrede
 
     try {
 
-        const result = await hub2bTenantCredentialCollection.insertOne( tenantCredentials )
+        const replacement = { ...tenantCredentials }
 
-        return result.ops[0] ? true : false
+        const options = {
+            upsert: true,
+            returnNewDocument: true
+        }
+
+        const result = await hub2bTenantCredentialCollection.findOneAndReplace( replacement, options )
+
+        return result.ok ? true : false
 
     } catch ( error ) {
         if ( error instanceof MongoError || error instanceof Error )
