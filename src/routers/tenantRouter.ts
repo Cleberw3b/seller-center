234392d0b027c111d7cd9957a86c10d4d5bba4e4
diogo-1630668567 <diogo.noroes@gmail.com>
2021-11-14 00:20:17 -0300

    feat: add tenants routers

--- src/routers/tenantRouter.ts
+++ src/routers/tenantRouter.ts
@@ -1,67 +1,86 @@
 import { Router } from 'express'
-import { validatePayloadCreateTenant, validatePayloadUpdateTenant, validatePayloadGetTenant } from '../utils/middlewares'
-import { Request, Response } from 'express'
-import { createTenant, updateHub2bTenant, getHub2bTenant } from '../services/hub2bTenantService'
-import { createHttpStatus, internalServerError, ok } from '../utils/httpStatus'
+import { celebrate, Segments, Joi } from 'celebrate';
+import TenantsController from '../controllers/TenantsController';
 
-const router = Router()
+const tenantRouter = Router()
+const tenantsController = new TenantsController();
 
 /**
  * POST -> Creates a tenant (Sellers)
  */
- router.post('/create', validatePayloadCreateTenant,
-    async (request: Request, response: Response) => {
-        const body = request.body
-        const tenant = await createTenant( body )
-      
-        if ( !tenant )
-            return response
-                .status( internalServerError.status )
-                .send( createHttpStatus( internalServerError ) )
-      
-        return response
-            .status( ok.status )
-            .send( tenant )
-    }
+ tenantRouter.post(
+  '/create',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      website: Joi.string().required(),
+      documentNumber: Joi.string().required(),
+      companyName: Joi.string().required(),
+      ownerName: Joi.string().required(),
+      ownerEmail: Joi.string().required(),
+      ownerPhoneNumber: Joi.string().required(),
+      idAgency: Joi.number().required(),
+      stateInscription: Joi.string().required(),
+      address: {
+          zipCode: Joi.string().required(),
+          street: Joi.string().required(),
+          neighborhood: Joi.string().required(),
+          number: Joi.number().required(),
+          city: Joi.string().required(),
+          state: Joi.string().required(),
+          country: Joi.string().required(),
+          reference: Joi.string()
+      }
+    },
+  }),
+  tenantsController.create,
 )
 
 /**
  * PUT -> Update a tenant (Seller)
  */
- router.put('/:idTenant', validatePayloadUpdateTenant,
-    async (request: Request, response: Response) => {
-        const body = request.body
-        body.idTenant = request.params.idTenant
-        const tenant = await updateHub2bTenant( body )
-      
-        if ( !tenant )
-            return response
-                .status( internalServerError.status )
-                .send( createHttpStatus( internalServerError ) )
-      
-        return response
-            .status( ok.status )
-            .send( tenant )
-    }
+ tenantRouter.put(
+  '/:idTenant',
+  celebrate({
+    [Segments.PARAMS]: {
+      idTenant: Joi.number().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      website: Joi.string().required(),
+      documentNumber: Joi.string().required(),
+      companyName: Joi.string().required(),
+      ownerName: Joi.string().required(),
+      ownerEmail: Joi.string().required(),
+      ownerPhoneNumber: Joi.string().required(),
+      idAgency: Joi.number().required(),
+      stateInscription: Joi.string().required(),
+      address: {
+          zipCode: Joi.string().required(),
+          street: Joi.string().required(),
+          neighborhood: Joi.string().required(),
+          number: Joi.number().required(),
+          city: Joi.string().required(),
+          state: Joi.string().required(),
+          country: Joi.string().required(),
+          reference: Joi.string()
+      }
+    },
+  }),
+  tenantsController.update,
 )
 
 /**
  * GET -> Retrieve tenant (Seller)
  */
- router.get('/:idTenant', validatePayloadGetTenant,
-    async (request: Request, response: Response) => {
-        const idTenant = request.params.idTenant
-        const tenant = await getHub2bTenant( idTenant )
-      
-        if ( !tenant )
-            return response
-                .status( internalServerError.status )
-                .send( createHttpStatus( internalServerError ) )
-      
-        return response
-            .status( ok.status )
-            .send( tenant )
-    }
+ tenantRouter.get(
+  '/:idTenant',
+  celebrate({
+    [Segments.PARAMS]: {
+      idTenant: Joi.number().required(),
+    },
+  }),
+  tenantsController.show,
 )
 
-export { router as tenantRouter }
+export { tenantRouter }
--- src/services/hub2bTenantService.ts
+++ src/services/hub2bTenantService.ts
@@ -8,7 +8,7 @@ import { saveUser } from "../repositories/hub2UserRepository"
 import { saveTenantCredential } from "../repositories/hub2TenantCredentialRepository"
 import { log } from "../utils/loggerUtil"
 import { getFunctionName } from "../utils/util"
-import { HUB2B_URL_V2, HUB2B_TENANT } from "../utils/consts"
+import { HUB2B_URL_V2 } from "../utils/consts"
 import { HUB2B_CREDENTIALS, renewAccessTokenHub2b } from "./hub2bAuhService"
 import { requestHub2B } from "./hub2bService"
 
@@ -18,17 +18,6 @@ import { requestHub2B } from "./hub2bService"
  * @param body
  */
 export const createTenant = async ( body: any ): Promise<HUB2B_Tenants | null> => {
-
-    if ( !body.idAgency ) {
-        const agency = await getTenantInHub2b(HUB2B_TENANT)
-
-        if ( !agency ) {
-            log( `Agency ${ body.idAgency } not found in the hub2b`, 'EVENT', getFunctionName(), 'ERROR' )
-            return null
-        }
-
-        body.idAgency = agency.idAgency
-    }
 
     const newTenant = await setupTenantsHub2b( body )
 
@@ -57,29 +46,6 @@ export const createTenant = async ( body: any ): Promise<HUB2B_Tenants | null> =
     log( `Tenant ${ newTenant.name } has been created.`, 'EVENT', getFunctionName() )
 
     return newTenant
-}
-
-/**
- * Get Tenant
- * 
- * @returns 
- */
- export const getTenantInHub2b = async (idTenant: any): Promise<HUB2B_Tenants | null> => {
-    await renewAccessTokenHub2b()
-
-    const SETUP_URL = HUB2B_URL_V2 + 
-      "/Setup/Tenants/" + idTenant + "?access_token=" + HUB2B_CREDENTIALS.access_token
-    
-    const response = await requestHub2B( SETUP_URL, 'GET' )
-    if ( !response ) return null
-
-    const tenant = response.data
-
-    tenant
-        ? log( "GET Tenant in hub2b success", "EVENT", getFunctionName() )
-        : log( "GET Tenant in hub2b error", "EVENT", getFunctionName(), "WARN" )
-
-    return tenant
 }
 
 /**
@@ -136,17 +102,6 @@ export const setupTenantsHub2b = async (body: any) => {
  * @param body
  */
  export const updateHub2bTenant = async ( body: any ): Promise<HUB2B_Tenants | null> => {
-
-    if ( !body.idAgency ) {
-        const agency = await getTenantInHub2b(HUB2B_TENANT)
-
-        if ( !agency ) {
-            log( `Agency ${ body.idAgency } not found in the hub2b`, 'EVENT', getFunctionName(), 'ERROR' )
-            return null
-        }
-
-        body.idAgency = agency.idAgency
-    }
 
     const tenant = await updateTenantsInHub2b( body )
 
